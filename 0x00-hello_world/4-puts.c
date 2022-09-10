b288b7f208b88c10735a7fa7b830e76694f7d5b0
Ogbononso <syeneo73@gmail.com>
2022-09-10 14:47:37 -0700

    commit it

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,17 +1,11 @@
 #include <stdio.h>
 /**
- *  * main - main block
- *   * Return: 0
- *    */
+ *  * main - Entry point
+ *   *
+ *    * Return: always 0
+ *     */
 int main(void)
 {
 		puts("\"Programming is like building a multilingual puzzle");
 			return (0);
 }
-Footer
-© 2022 GitHub, Inc.
-Footer navigation
-Terms
-Privacy
-Security
-Status
